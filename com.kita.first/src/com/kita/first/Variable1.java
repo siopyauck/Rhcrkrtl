ce28a13d52c508559b79c9752e3bf3efdeda7ed8
siopyauck <siopyauck@gmail.com>
2020-12-29 15:06:09 +0900

    20.12.22.~20.12.29. JAVA1 백업

---
+++ com.kita.first/src/com/kita/first/Variable1.java
@@ -0,0 +1,18 @@
+package com.kita.first;
+
+public class Variable1 {
+	public static void main(String[] args) {
+		char v1;
+		v1 = 'A';
+		
+		byte v2;
+		short v3;
+		int v4;
+		long v5;
+		
+		float v5;
+		double v7;
+		
+		boolean v8;
+	}
+}
---
+++ com.kita.first/src/com/kita/first/practice/Practice1.java
@@ -0,0 +1,7 @@
+package com.kita.first.practice;
+
+public class Practice1 {
+	public static void main(String[] args) {
+		System.out.print("안녕하세요.\n저는 김땡땡입니다.\n만나서 반값습니다.");
+	}
+}
